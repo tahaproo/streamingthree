037a8159d0c60dd98e8353950ecff6bd44427853
tahaproo <44582827+tahaproo@users.noreply.github.com>
2018-11-23 21:15:45 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "3";
 var adminprefix = '3'
 
-const developers = ["395704557293797377"]
+const developers = ["389349268357513217"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
